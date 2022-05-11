2ffb60a38d09bb22755dcf20c3cb6312932691b2
rusterz <aaadev32@gmail.com>
2022-05-10 19:03:18 -0700

    probably nothing... stuck on addToLibrary function for 2ish hours but committing in case i accomplished something and forgot

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-let myLibrary = {
-    title: 'test',
-    author: 'test',
-    pages: 123,
-    read: 'No'
+let testBook = {
+    book1: {title: 'test', author: 'test',pages: 123, read: 'No'}
 };
-let newBook = null;
+let myLibrary = [testBook];
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
+    console.log(Book);
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    let newBook = Book(title, author, pages, read);
+    let newBook = {};
+    newBook = new Book(title, author, pages, read);
+
     myLibrary.push.newBook;
-    //let Book1 = Object.Create(Book);
+    console.log(newBook);
+    console.log(myLibrary);
+    i++;
 }
 
 function listBooks() {
@@ -41,13 +43,13 @@ function popup() {
     //temporary(?) check for null to silence console errors
     if (close == null) {
         closePopup();
-        addBookToLibrary(title, author, pages, read);
+        addBookToLibrary(document.getElementById('title').value, document.getElementById('author').value, document.getElementById('pages').value, document.getElementById('read').value);
         return 1;
     }
-    
+
     document.getElementById('close-popup').id = 'open-popup';
     document.getElementById('add-book').textContent = 'Submit';
-    for(i = 0; i < length; i++) {
+    for (i = 0; i < length; i++) {
         bookForm[i].style.display = 'block';
     }
 
@@ -61,11 +63,13 @@ function closePopup() {
     document.getElementById('open-popup').id = 'close-popup';
     document.getElementById('add-book').textContent = 'Add Book';
     document.getElementsByClassName('input-label').display = 'none'
-    for(i = 0; i < length; i++) {
+    for (i = 0; i < length; i++) {
         bookForm[i].style.display = 'none';
     }
     console.log('bye');
 }
 
 
-//ended session by adding addNewBook() on line 44. figure out if it is properly adding books to the myLibrary object (havent tested) 5/9/22
+//newBook succesfully gets turned into an object by new Book constructor and with the correct values but when pushed to myLibrary it doesnt get stored
+//although by using the testBook variable which is constructed the same way and manually entering the object it gets stored properly
+//  5/10/22 left off at addToLibrary function: find out how to push an object variable into an array
